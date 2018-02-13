484c315a737cdccb2f8dc6f47ab1d58785ad5008
SugaLo <33042707+Sugalo23@users.noreply.github.com>
2018-02-13 15:06:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+$.getScript( "questions.js", function( data, textStatus, jqxhr ) {
+  console.log( data ); // Data returned
+  console.log( textStatus ); // Success
+  console.log( jqxhr.status ); // 200
+  console.log( "Load was performed." );
+});
+
 $(document).ready(function() {
   $(".begin").click(function() {
     $("#main").hide();
